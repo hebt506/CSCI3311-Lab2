c6111aa8aaa61762c7f3a0320288df83bef1c726
Jinghong <hebt@bc.edu>
2021-09-17 02:05:21 -0400

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// TODO: load the dataset 
+
+function filterData(category) {
+
+	/* **************************************************
+	 *
+	 * TODO: filter attractions by the selected category
+	 * TODO: filter top 5 attractions
+	 *
+	 * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
+	 *
+	 * renderBarChart(data)
+	 *
+	 * - 'data' must be an array of JSON objects
+	 * - the max. length of 'data' is 5
+	 *
+	 * **************************************************/
+
+}
+
+// TODO: Define an event listener for the dropdown menu
+//       Call filterData with the selected category
